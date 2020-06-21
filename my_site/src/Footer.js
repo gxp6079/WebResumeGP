a3aff2c7c50cd8ddffcf8be7ed6dad272bcae4fc
gihpuccini <gxp6079@g.rit.edu>
2020-06-21 18:21:39 -0400

    footer

--- my_site/src/Footer.js
+++ my_site/src/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button, ButtonGroup } from "reactstrap";
+import linkedin from "./images/linkedin_logo.png";
+import github from "./images/github_logo.png";
 
 function Footer() {
   return (
     <>
-      <footer className="footer-gray">
+      <div className="footer-gray">
         <ButtonGroup className="footer-button-cont">
           <Button
             color="link"
@@ -39,7 +41,18 @@ function Footer() {
             Industry Experience
           </Button>
         </ButtonGroup>
-      </footer>
+        <ButtonGroup className="footer-link-cont">
+          <a
+            href="https://www.linkedin.com/in/giovana-puccini-52059213b/"
+            className="social_logo"
+          >
+            <img src={linkedin} className="img-fluid" />
+          </a>
+          <a href="https://github.com/gxp6079" className="social_logo">
+            <img src={github} className="img-fluid" />
+          </a>
+        </ButtonGroup>
+      </div>
     </>
   );
 }
